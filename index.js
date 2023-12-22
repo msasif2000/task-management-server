6815e1aac65e5dff6d9d5864f71b813209b95521
Asif <msasifiiuc@gmail.com>
2023-12-22 08:16:01 +0600

    delete task, update task operation done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,22 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//middlewares
-const verifyToken = (req, res, next) => {
-    console.log('verify token', req.headers.authorization);
-    if (!req.headers.authorization) {
-        return res.status(401).send('Unauthorized request');
-    }
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send('Unauthorized request');
-        }
-        req.decoded = decoded;
-        next();
-    })
-    //next();
-}
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.alzohbu.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,6 +31,7 @@ dbConnect()
 
 
 const userCollection = client.db("taskManagementDB").collection("users");
+const tasksCollection = client.db("taskManagementDB").collection("tasks");
 
 
 app.get('/', (req, res) => {
@@ -65,8 +49,55 @@ app.post('/users',  async (req, res) => {
     res.send(result);
 })
 
-app.get('/users', verifyToken, async (req, res) => {
-    const result = await userCollection.find({}).toArray();
+
+app.get('/usersLogin', async (req, res) => {
+    const email = req.query.email;
+    const query = { email: email };
+    const result = await userCollection.findOne(query);
+    res.send(result);
+});
+
+app.post('/createTask',  async(req, res) => {
+    const task = req.body;
+    const result = await tasksCollection.insertOne(task);
+    res.send(result);
+})
+
+app.get('/myTasks/:email', async(req, res)=> 
+{
+    const email = req.params.email;
+    const query = { email: email};
+    const result = await tasksCollection.find(query).toArray();
+    res.send(result);
+})
+
+app.delete('/deleteTask/:id', async(req, res)=> 
+{
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id)};
+    const result = await tasksCollection.deleteOne(query);
+    res.send(result);
+})
+app.get('/myTask/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id)};
+    const result = await tasksCollection.findOne(query);
+    res.send(result);
+})
+
+app.put('/updateTask/:id', async(req, res) => {
+    const id = req.params.id;
+    const task = req.body;
+    const query = { _id: new ObjectId(id)};
+    const updateDoc = {
+        $set: {
+            title: task.title,
+            deadline: task.deadline,
+            priority: task.priority,
+            details: task.details,
+        },
+    };
+    const result = await tasksCollection.updateOne(query, updateDoc);
     res.send(result);
 })
 
